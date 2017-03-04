5b883813ece812c84ffaa9add94049205896cd76
juniornelson123 <juniornelson123@gmail.com>
2017-03-04 13:46:38 -0300

    n talk

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,16 +1,38 @@
 module.exports = function(io){
 	var	sockets	=	io.sockets;
 	console.log("conexão sockets iniciada")
-
+	var crypto = require("crypto")
 	sockets.on('connection', function(client){
 	var session = client.handshake.session 
 	
 	var usuario = session.usuario
 		client.on('send-server', function(msg){
 			console.log("recebendo uma mensagen no server")
+			var sala = session.sala
+			var data = {email: usuario.email, sala: sala}
 			var msg = "<b>"+usuario.nome+":</b> "+msg+"</br>"
-			client.emit('send-client', msg);
-			client.broadcast.emit('send-client', msg);
+
+			client.broadcast.emit('new-message', data)
+			client.emit('send-client', msg)
+			client.in(sala).emit('send-client', msg);
+		})
+
+		client.on("join", function(sala){
+			if (!sala) {
+				var timestamp = new Date().toString()
+				var md5 = crypto.createHash('md5')
+				sala = md5.update(timestamp).digest('hex')
+
+			}
+
+			session.sala = sala
+			client.join(sala)
+		})
+
+		client.on('disconnect', function(){
+			client.leave(session.sala)
 		})
 	})
+
+
 }
